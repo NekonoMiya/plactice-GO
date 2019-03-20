4dc2c2d02de3be9243c67d816fbc0d1e8eb48b05
NekonoMiya <miyasanneko@gmail.com>
2019-03-20 15:14:37 +0900

    剰余

--- hoge.go
+++ hoge.go
@@ -8,10 +8,20 @@ func pl(x int, y int) int {
 
 func main(){
 	fmt.Println(pl(3,4))
+
 	a, b := i("hello","world")
 	fmt.Println(a,b)
+
+	c, d := kw(4,2)
+	fmt.Println("剰余演算",c,d)
 }
 
 func i(x,y string) (string, string) {
 	return y, x
+}
+
+func kw(x, y int) (a, b int){
+	a = x*y
+	b = x/y
+	return 
 }
